755acc23ba608abfe3ebb693170c9f00a2709a6f
camicastro <camicastro.11.94@gmail.com>
2023-06-15 17:31:16 -0300

    Listo para rendir HC

--- 27- SimulacionHC.js
+++ 27- SimulacionHC.js
@@ -59,6 +59,27 @@ function soloNumeros(array) {
     return false;
   }
 
+  function buscarAmigo(amigos, nombre) {
+    // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
+    // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
+    // recibe un string llamado 'nombre'.
+    // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+    // Ej:
+    //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+    //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
+  
+    // Tu código aca:
+  
+    let amigoEncontrado = {}
+  
+    for(let i = 0; i < amigos.length; i++){
+      if(amigos[i].nombre === nombre){
+        amigoEncontrado = amigos[i];
+      }
+    }  
+    return amigoEncontrado;
+  }
+
   function pluck(array, propiedad) {
     // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' y el nombre de una
     // propiedad.
